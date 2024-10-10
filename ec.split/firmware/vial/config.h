04a8b0c606a4af31ad59bf08641d65b481d67197
eli2c <eliaszchyla12@gmail.com>
2024-10-10 20:15:50 +0200

    Add files via upload

---
+++ ec.split/firmware/vial/config.h
@@ -0,0 +1,28 @@
+
+// Copyright 2024 no (@no)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+
+
+
+
+
+/*
+
+#define
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
